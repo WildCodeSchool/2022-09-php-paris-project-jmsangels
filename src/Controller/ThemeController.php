de20488b791930b4b2948eed86424da460b1ef6f
Sonia Baptista <sonia.baptista095@gmail.com>
2022-11-16 11:49:54 +0100

    create page theme with subject menu

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Model\SubjectManager;
 use App\Model\ThemeManager;
 
 class ThemeController extends AbstractController
@@ -18,6 +19,31 @@ class ThemeController extends AbstractController
             [
                 'headerTitle' => self::HEADERTITLE,
                 'themes' => $themeManager->selectAll()
+            ]
+        );
+    }
+
+    public function show(string $themeId): string
+    {
+
+        if (is_numeric($themeId) == null) {
+            header("Location: /");
+
+        }
+
+
+        $themeManager = new ThemeManager();
+        $theme = $themeManager->selectOneById($themeId);
+        $name = $theme['name'];
+
+        $subjectManager = new SubjectManager();
+        $subjects = $subjectManager->selectAllByThemeId($themeId);
+
+        return $this->twig->render(
+            'Notion/index.html.twig',
+            [
+                'headerTitle' => $name,
+                'subjects' => $subjects
             ]
         );
     }
---
+++ src/Model/SubjectManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class SubjectManager extends AbstractManager
+{
+    public const TABLE = 'subject';
+
+    public function selectAllByThemeId(int $theme_id): array
+    {
+
+        $statement = $this->pdo->prepare("SELECT * FROM " . self::TABLE . " WHERE `theme_id` = :idtheme");
+        $statement->bindValue('idtheme', $theme_id, PDO::PARAM_INT);
+        $statement->execute();
+
+
+        return $statement->fetchAll();
+
+    }
+
+
+}
