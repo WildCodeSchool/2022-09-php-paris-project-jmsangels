94af9a926493c9cae117662ab1775b2a7e125977
Sonia Baptista <sonia.baptista095@gmail.com>
2022-11-17 10:33:11 +0100

    modify pr

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -7,18 +7,21 @@ use App\Model\ThemeManager;
 
 class ThemeController extends AbstractController
 {
+    private $themeManager;
     public const HEADERTITLE = 'KNOWLEDGE';
     /**
      * Display home page
      */
     public function index(): string
     {
-        $themeManager = new ThemeManager();
+
+        $this->themeManager = new ThemeManager();
+
         return $this->twig->render(
             'Theme/index.html.twig',
             [
                 'headerTitle' => self::HEADERTITLE,
-                'themes' => $themeManager->selectAll()
+                'themes' => $this->themeManager->selectAll()
             ]
         );
     }
@@ -32,24 +35,15 @@ class ThemeController extends AbstractController
 
 
         $themeManager = new ThemeManager();
-<<<<<<< HEAD
         $theme = $themeManager->selectOneById((int) $themeId);
-        $name = $theme['name'];
-
+    
         $subjectManager = new SubjectManager();
         $subjects = $subjectManager->selectAllByThemeId((int) $themeId);
-=======
-        $theme = $themeManager->selectOneById((int)$themeId);
-        $name = $theme['name'];
-
-        $subjectManager = new SubjectManager();
-        $subjects = $subjectManager->selectAllByThemeId((int)$themeId);
->>>>>>> c3376e7230aa0c46ff97039daff0cbe8554fa1bf
 
         return $this->twig->render(
             'Notion/index.html.twig',
             [
-                'headerTitle' => $name,
+                'headerTitle' => $theme['name'],
                 'subjects' => $subjects
             ]
         );
