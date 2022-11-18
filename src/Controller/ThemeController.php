9b6b50c416c3a67704504fcdfc54588f3e66c5f6
Anastasiya_Fernandez <anastasiya.ustych@gmail.com>
2022-11-18 14:31:03 +0100

    PR update

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -7,16 +7,17 @@ use App\Model\ThemeManager;
 
 class ThemeController extends AbstractController
 {
-    private $themeManager;
+    private ThemeManager $themeManager;
     public const HEADERTITLE = 'KNOWLEDGE';
     /**
      * Display home page
      */
+    public function __construct()
+    {
+        $this->themeManager = new ThemeManager();
+    }
     public function index(): string
     {
-
-        $this->themeManager = new ThemeManager();
-
         return $this->twig->render(
             'Theme/index.html.twig',
             [
